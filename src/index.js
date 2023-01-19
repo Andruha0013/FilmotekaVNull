05eb147bb2af1604f3ac151222feded4ba12d0fa
Andruha0013 <andruha0013@gmail.com>
2023-01-19 16:13:50 +0200

    test7

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import './js/spinner';
 import './sass/main.scss';
-// import './js/modalFilmCard.js';
+//import './js/modalFilmCard.js';
 import './js/modalFilmCard_V2';
-import './js/cardFetc';
+//import './js/cardFetc';
 //import './js/teamLightbox';
 //import './js/pagination.js';
 //import './js/registrationModal';
 // import '../node_modules/basiclightbox/dist/basicLightbox.min.css';
-import './js/keyWordSearch';
+//import './js/keyWordSearch';
 //import 'regenerator-runtime/runtime.js';
 //import './js/trailers';
 import './js/btn-upp';
